adc400938b85d31bb78f6754c3f3eaa9ac079ce0
Zach Oda <zjodachow@gmail.com>
2022-08-16 11:09:24 -0400

    updated add/delete

--- server.js
+++ server.js
@@ -19,10 +19,37 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
   );
+//Get all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+// //Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id=10`, (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// // Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// VALUES (?, ?, ?, ?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
