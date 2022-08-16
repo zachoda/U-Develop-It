c6bd2b6cc1ff8d59e53426cb45c4f1c1cacaa9b0
Zach Oda <zjodachow@gmail.com>
2022-08-16 18:22:58 -0400

    added parties table

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
-const mysql = require("mysql2");
-const express = require("express");
-const inputCheck = require("./utils/inputCheck");
+const express = require('express');
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//EXPRESS.JS Middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
   {
-    host: "localhost",
+    host: 'localhost',
     // Your MySQL username,
-    user: "root",
+    user: 'root',
     // Your MySQL password
-    password: "password",
-    database: "election",
+    password: 'password',
+    database: 'election'
   },
-  console.log("Connected to the election database.")
+  console.log('Connected to the election database.')
 );
-//Get all candidates
-app.get("/api/candidates", (req, res) => {
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
   const sql = `SELECT * FROM candidates`;
 
   db.query(sql, (err, rows) => {
@@ -30,15 +32,15 @@ app.get("/api/candidates", (req, res) => {
       return;
     }
     res.json({
-      message: "SUCCESS",
-      data: rows,
+      message: 'success',
+      data: rows
     });
   });
 });
 
-//Get a single candidate
-app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates where id= ?`;
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -47,14 +49,14 @@ app.get("/api/candidate/:id", (req, res) => {
       return;
     }
     res.json({
-      message: "SUCCESS",
-      data: row,
+      message: 'success',
+      data: row
     });
   });
 });
 
 // Delete a candidate
-app.delete("/api/candidate/:id", (req, res) => {
+app.delete('/api/candidate/:id', (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
   const params = [req.params.id];
 
@@ -63,39 +65,45 @@ app.delete("/api/candidate/:id", (req, res) => {
       res.statusMessage(400).json({ error: res.message });
     } else if (!result.affectedRows) {
       res.json({
-        message: "Candidate not found",
+        message: 'Candidate not found'
       });
     } else {
       res.json({
-        message: "deleted",
+        message: 'deleted',
         changes: result.affectedRows,
-        id: req.params.id,
+        id: req.params.id
       });
     }
   });
 });
 
 // Create a candidate
-app.post("/api/candidate", ({body}, res) => {
-    const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
-if(errors) {
-    res.status(400).json({error: errors});
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
     return;
-} 
-const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
+  }
 
-db.query(sql, params, (err, result) => {
-if (err) {
-  res.status(400).json({ error: err.message });
-  return;
-}
-res.json({
-  message: 'success',
-  data: body
-});
-});
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
 });
 
 // Default response for any other request (Not Found)
@@ -104,5 +112,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`SERVER now running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
